faf4d1ec0c22c4c4b77e6aea7cd45f3d1cfcf608
Alcia <alicja.grochowina002@interia.pl>
2023-04-10 15:58:07 +0200

    Zmiana silnika z Godot 3.2 na Godot 4. Przepisanie skryptów na GDScript 2. Zmiana skryptu dasha gracza. Dodanie efektów przy pokonaniu przeciwnika. Usunięcie Tail.tscn i DashTrail.tscn.

--- src/enemy/enemy.gd
+++ src/enemy/enemy.gd
@@ -1,11 +1,23 @@
 extends Area2D
 
+func destroy():
+	print("enemy dead")
+	$Sprite2D.hide() #włącz animację
+	$CollisionShape2D.disabled = true
+	$GPUParticles2D.emitting = true
+	
+	await get_tree().create_timer(2).timeout #usuń
+	queue_free()
+
+func _input(event):
+	if event is InputEventKey and event.is_action_pressed("space"):
+		destroy()
+
 func _on_enemy_body_entered(body):
-	if body.dashing != "":
-		print("enemy dead")
+	print(body.dashing)
+	if body.dashing == true:
 		body.jump_count = 2
 		body.dash_count = 1
-		hide()
-		queue_free()
+		call_deferred("destroy")
 	else:
 		body.hit()
--- src/player/Player.gd
+++ src/player/Player.gd
@@ -1,33 +1,35 @@
 #naprawić flip dasha kiedy dashujesz w przeciwną stronę w którą idziesz
 
-extends KinematicBody2D
+extends CharacterBody2D
 
 const PX_PER_SEC_DASH = 0.1/100
 const GRAVITY = 450.0
 const WALK_SPEED = 80 #150
 
-var velocity = Vector2(0,0)
+#var velocity = Vector2(0,0)
 
 var jump_count = 2
 var dash_count = 1
-var dashing = ""
+var dashing = false
 
 ###do testów###
-onready var debug_labels = get_parent().find_node("debug").get_child(0)
+@onready var debug_labels = get_parent().find_child("debug").get_child(0)
 
 func _physics_process(delta):
-	debug_labels.get_child(4).text = str(Engine.get_frames_per_second())
+	debug_labels.get_child(4).text = str(Engine.get_frames_per_second()) #statystki debugowania
 	debug_labels.get_child(3).text = str(Input.get_accelerometer().x)
 	debug_labels.get_child(1).text = str(dash_count)
-	if is_on_floor() and dashing == "":
+	if is_on_floor() and dashing == false: #reset dasha i skoku
 		jump_count = 2
 		dash_count = 1
 		debug_labels.get_child(0).text = "on floor"
 	else:
 		debug_labels.get_child(0).text = "not on floor"
 	
-	if velocity.y < 240: #max prędkość spadania
+	if velocity.y < 240 and !dashing: #max prędkość spadania
 		velocity.y += delta * GRAVITY
+	elif dashing and velocity.y >= 0:
+		velocity.y = 0
 	
 	if abs(Input.get_accelerometer().x) > 0.8: #obliczanie szybkości i kierunku
 		velocity.x = Input.get_accelerometer().x * WALK_SPEED
@@ -35,67 +37,53 @@ func _physics_process(delta):
 		velocity.x = 0
 	
 	if velocity.x > 0: #prawo
-		flip_tail(false)
 		velocity.x -= 15 #podczas chodzenia, zwolnij postać o 15px, dzięki temu (i ifie wyżej) postać może stanąć w miejscu
 	elif velocity.x < 0:
-		flip_tail(true)
 		velocity.x += 15 #podczas chodzenia, zwolnij postać o 15px, dzięki temu (i ifie wyżej) postać może stanąć w miejscu
 	
 	#do testów#
 	if Input.is_action_pressed("ui_left"):
 		velocity.x = -WALK_SPEED * 2
-		
-		flip_tail(true)
 	elif Input.is_action_pressed("ui_right"):
 		velocity.x =  WALK_SPEED * 2
-		
-		flip_tail(false)
 	elif Input.is_action_just_pressed("ui_up"):
 		jump()
 	
-	if $AnimatedSprite.animation != "idle" and $AnimatedSprite.playing == false:
-		$AnimationPlayer.stop()
-		$AnimatedSprite.playing = true
+	if $AnimatedSprite2D.animation != "idle" and $AnimatedSprite2D.is_playing() == true:
+		$AnimationPlayer.pause()
 	
 	#ustaw animacje
-	if dashing == "":
+	if dashing == false:
 		if velocity.y > 0 and !is_on_floor():
-			$AnimatedSprite.animation = "fall"
+			$AnimatedSprite2D.play("fall")
 		if velocity.y < 0 and !is_on_floor():
-			$AnimatedSprite.animation = "jump"
+			$AnimatedSprite2D.play("jump")
 		elif velocity.x != 0 and is_on_floor():
-			$AnimatedSprite.animation = "walk"
-		elif velocity.x == 0 and is_on_floor():
-			$AnimatedSprite.playing = false
-			$AnimatedSprite.animation = "idle"
+			$AnimatedSprite2D.play("walk")
+			if velocity.x > 0:
+				$AnimatedSprite2D.flip_h = true
+			else:
+				$AnimatedSprite2D.flip_h = false
+		elif velocity.x == 0 and is_on_floor() and $AnimationPlayer.current_animation != "idle":
+			$AnimatedSprite2D.animation = "idle"
+			$AnimatedSprite2D.stop()
 			$AnimationPlayer.play("idle")
 	
 # warning-ignore:return_value_discarded
-	move_and_slide(velocity, Vector2(0, -1))
+	set_velocity(velocity)
+	set_up_direction(Vector2(0, -1))
+	move_and_slide()
 
 func hit():
 	self.modulate = Color(1,0,0)
-	yield(get_tree().create_timer(0.5), "timeout")
+	await get_tree().create_timer(0.5).timeout
 	self.modulate = Color(1,1,1)
-
-# warning-ignore:unused_argument
-# warning-ignore:unused_argument
-func _on_Tween_tween_started(object, key):
-	self.modulate = Color(0,1,0)
-	$Timer.start(0.2)
-
-func _on_Timer_timeout():
-	if dashing == "lewo":
-		$Tail.position = Vector2(6,0)
-	elif dashing == "prawo":
-		$Tail.position = Vector2(-6,0)
-	dashing = ""
-	self.modulate = Color(1,1,1)
-	$Timer.stop()
 
 #zmienne do sprawdzenia kierunku swipe'a
 var swipe_start = Vector2(0,0)
 var swipe_end = Vector2(0,0)
+var direction: int #direction 2 = góra, 1 = prawo, 0 = lewo
+
 #sprawdzenie swipe'a i uruchomienie dasha
 func _input(event):
 	if event is InputEventScreenTouch:
@@ -115,12 +103,17 @@ func _input(event):
 				if y_diff < 10: #grace period
 					jump()
 					return
-				dash(2) #jeżeli różnica pomiędzy zmiennymi y jest większa niż pomiędzy x to dash w górę
+				direction = 2
+				dash() #jeżeli różnica pomiędzy zmiennymi y jest większa niż pomiędzy x to dash w górę
 			elif abs(x_diff) < 10 : #grace period
 				jump()
 				return
-			elif x_diff > 0: dash(0) #jeżeli różnica pomiędzy x jest większa od 0 to dashuj w lewo
-			elif x_diff < 0: dash(1) #jeżeli różnica pomiędzy x jest mniesza od 0 to dashuj w prawo
+			elif x_diff > 0:
+				direction = 0
+				dash() #jeżeli różnica pomiędzy x jest większa od 0 to dashuj w lewo
+			elif x_diff < 0:
+				direction = 1
+				dash() #jeżeli różnica pomiędzy x jest mniesza od 0 to dashuj w prawo
 
 func jump():
 	if !is_on_floor() and jump_count > 0:
@@ -136,70 +129,84 @@ func jump():
 var touching_left_wall = false
 var touching_right_wall = false
 var touching_ceiling = false
+var tween: Tween
 
-#ta funkcja tylko kalkuluje o ile powinien zostać player przeniesiony i z jaką prędkością
-#czasem trwania dasha zajmuje się timer
-#direction 2 = góra, 1 = prawo, 0 = lewo
-func dash(direction):
-	var stara_pozycja = Vector2(0,0)
-	var nowa_pozycja = Vector2(0,0)
-	var time = 0.1
+#całkowity czas trwania dasha
+func dashTimer():
+	self.modulate = Color(0,1,0)
+	await get_tree().create_timer(0.15).timeout
+	dashing = false
+	self.modulate = Color(1,1,1)
 	
-	if dash_count > 0 and direction == 0:
-		dashing = "lewo"
-		$AnimatedSprite.animation = "dash"
-		flip_tail(true)
-		$DashParticles.emitting = true
+	velocity.x = 0
+	velocity.y = 0
+	
+	if direction == 2:
+		velocity.y -= 90
+
+#ta funkcja kalkuluje ile pixeli powinien zostać przeniesiony gracz
+#direction 2 = góra, 1 = prawo, 0 = lewo
+#func dash():
+#	var nowa_pozycja = Vector2(0,0)
+#	var time = 0.1
+#	tween = create_tween()
+#	tween.stop()
+#
+#	if dash_count > 0:
+#		dashing = true
+#		if direction == 0 or direction == 1:
+#			$AnimatedSprite2D.play("dash")
+#		match direction:
+#			0:
+#				nowa_pozycja = Vector2(position.x-100, position.y)
+#				if touching_left_wall: #jeżeli nowa pozycja wyjdzie poza granicę
+#					nowa_pozycja.x = position.x #to nie poruszaj
+#			1:
+#				nowa_pozycja = Vector2(position.x+100, position.y)
+#				if touching_right_wall: #jeżeli nowa pozycja wyjdzie poza granicę
+#					nowa_pozycja.x = position.x #to nie poruszaj
+#			2:
+#				$AnimatedSprite2D.animation = "jump"
+#				nowa_pozycja = Vector2(position.x, position.y-100)
+#				if touching_ceiling: #jeżeli nowa pozycja wyjdzie poza granicę
+#					nowa_pozycja.y = position.y #to nie poruszaj
+#
+#		tween.tween_property(self, "position", nowa_pozycja, time).set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_OUT)
+#		tween.play()
+#		dash_count -= 1
+#
+#		dashTimer()
+#
+#		if direction == 2: #reset grawitacji
+#			velocity.y = -200
+#		else:
+#			velocity.y = -50
+
+func dash():
+	if dash_count > 0:
+		dashing = true
+		if direction == 0 or direction == 1:
+			$AnimatedSprite2D.play("dash")
+		match direction:
+			0:
+				velocity.x -= 900
+			1:
+				velocity.x += 900
+			2:
+				$AnimatedSprite2D.animation = "jump"
+				velocity.y -= 900
+				print(velocity.y)
+				pass
 		
-		stara_pozycja = position
-		nowa_pozycja = Vector2(stara_pozycja.x-100, stara_pozycja.y)
-		
-		if touching_left_wall: #jeżeli nowa pozycja wyjdzie poza granicę
-			nowa_pozycja.x = stara_pozycja.x #to nie poruszaj
-		
-		$Tween.interpolate_property(self, "position", stara_pozycja, nowa_pozycja, time, Tween.TRANS_LINEAR, Tween.EASE_OUT)
-		$Tween.start()
-		
-		velocity.y = -50 #reset grawitacji
 		dash_count -= 1
-		
-	elif dash_count > 0 and direction == 1:
-		dashing = "prawo"
-		$AnimatedSprite.animation = "dash"
-		flip_tail(false)
-		
-		stara_pozycja = position
-		nowa_pozycja = Vector2(stara_pozycja.x+100, stara_pozycja.y)
-		
-		if touching_right_wall: #jeżeli nowa pozycja wyjdzie poza granicę
-			nowa_pozycja.x = stara_pozycja.x #to nie poruszaj
-		
-		$Tween.interpolate_property(self, "position", stara_pozycja, nowa_pozycja, time, Tween.TRANS_LINEAR, Tween.EASE_OUT)
-		$Tween.start()
-		
-		velocity.y = -50 #reset grawitacji
-		dash_count -= 1
-		
-	elif dash_count > 0 and direction == 2:
-		dashing = "góra"
-		$AnimatedSprite.animation = "jump"
-		
-		if touching_ceiling:
-			$Tween.interpolate_property(self, "position:y", position.y, position.y, 0.1, Tween.TRANS_LINEAR, Tween.EASE_OUT)
-			$Tween.start()
-		else:
-			$Tween.interpolate_property(self, "position:y", position.y, position.y-100, 0.1, Tween.TRANS_LINEAR, Tween.EASE_OUT)
-			$Tween.start()
-		
-		velocity.y = -200
-		dash_count -= 1
+		dashTimer()
 
 #zatrzymywanie się na ścianach 
 func _on_up_stop_body_entered(body):
 	if body.name != "player":
 		touching_ceiling = true
-		if dashing == "góra":
-			$Tween.stop_all()
+		if tween and direction == 2:
+			tween.kill()
 
 func _on_up_stop_body_exited(body):
 	if body.name != "player":
@@ -208,8 +215,8 @@ func _on_up_stop_body_exited(body):
 func _on_left_stop_body_entered(body):
 	if body.name != "player":
 		touching_left_wall = true
-		if dashing == "lewo":
-			$Tween.stop_all()
+		if tween and direction == 0:
+			tween.kill()
 
 func _on_left_stop_body_exited(body):
 	if body.name != "player":
@@ -218,24 +225,11 @@ func _on_left_stop_body_exited(body):
 func _on_right_stop_body_entered(body):
 	if body.name != "player":
 		touching_right_wall = true
-		if dashing == "prawo":
-			$Tween.stop_all()
+		if tween and direction == 1:
+			print("xd")
+			tween.kill()
 
 func _on_right_stop_body_exited(body):
 	if body.name != "player":
 		touching_right_wall = false
 
-#zmienia parametry symulacji ogona i odbija w pione sprite
-func flip_tail(walking_left):
-	if walking_left == false and $AnimatedSprite.flip_h == false:
-		$AnimatedSprite.set_flip_h(true)
-		$Tail.position = Vector2(-6,0)
-		$Tail/Particles2D.process_material.direction = Vector3(-1, 1, 0)
-		$Tail/Particles2D.process_material.gravity = Vector3(-40, -40, 0)
-		$Tail/Particles2D.process_material.tangential_accel = 20
-	elif walking_left == true and $AnimatedSprite.flip_h == true:
-		$AnimatedSprite.set_flip_h(false)
-		$Tail.position = Vector2(6,0)
-		$Tail/Particles2D.process_material.direction = Vector3(1, 1, 0)
-		$Tail/Particles2D.process_material.gravity = Vector3(40, -40, 0)
-		$Tail/Particles2D.process_material.tangential_accel = -20
